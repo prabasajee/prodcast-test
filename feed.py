e6b59f085001f6134ee7888c37a08d2be48ebb76
prabasajee <prabaharansajeevan23@gmail.com>
2025-06-26 13:07:45 +0530

    feed .py change

--- feed.py
+++ feed.py
@@ -1,7 +1,7 @@
 import yaml
 import xml.etree.ElementTree as xml_tree
 
-with open('prodcast-test/feed.yaml', 'r') as file:
+with open('feed.yaml', 'r') as file:
     yaml_data = yaml.safe_load(file)
 
 rss_element = xml_tree.Element('rss', {
